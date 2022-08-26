17788d86a5fe47cfed214d343c64fdc9e0c8f080
Sebastián Hidalgo Vargas <sebas-01-hv@hotmail.es>
2022-08-25 18:57:18 -0600

    Add QuickSort Algo

---
+++ PagedArray.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <string.h> 
+
+class PagedArray{
+    public 
+};
--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 //Tarea extraclase Sebastian Hidalgo Vargas
+#include "SortingAlgos.cpp"
 
+int main(){
 
+    int s[]={25,12,1,45,28,6,3,8,4,9,10,69,420,287,5};
+    int n=sizeof(s)/sizeof(s[0]);
+    quickSort(s,0,n-1);
+    cout <<"Sorted array: \n";
+    showArray(s,n);
 
-void main(){}
+    return 0;
+}
